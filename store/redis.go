98d1208894e2f35b36c5e8941e6131d00a37f836
Elahe Dastan <elahe.dstn@gmail.com>
2020-08-12 12:12:30 +0430

    Add communication with redis

--- store/redis.go
+++ store/redis.go
@@ -1,1 +1,35 @@
 package store
+
+import (
+	"fmt"
+	"github.com/gomodule/redigo/redis"
+	"log"
+	"strconv"
+)
+
+type Redis struct {
+	Conn    redis.Conn
+	Counter int
+}
+
+func New(r redis.Conn) Redis {
+	return Redis{Conn: r, Counter: 0}
+}
+
+func (redis *Redis) Insert(value string) {
+	_, err := redis.Conn.Do("SET", "eldis" + strconv.Itoa(redis.Counter), value)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	redis.Counter++
+}
+
+func (redis *Redis) Fetch(id int) string {
+	value, err := redis.Conn.Do("GET", "eldis" + strconv.Itoa(id))
+	if err != nil {
+		log.Print("This id doesn't exist")
+	}
+
+	return value
+}
