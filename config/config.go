4f1a025afbab79cade0f9f27209af9f9140877d9
Elahe Dastan <elahe.dstn@gmail.com>
2020-08-12 10:51:26 +0430

    Add config

--- config/config.go
+++ config/config.go
@@ -1,1 +1,46 @@
 package config
+
+import (
+	"bytes"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Redis Redis `mapstructure:"redis"`
+}
+
+type Redis struct {
+	Addr string `mapstructure:"addr"`
+}
+
+func Read(filename string) Config {
+	viper.AddConfigPath(".")
+	viper.SetConfigType("yml")
+
+	if err := viper.ReadConfig(bytes.NewBufferString(Default)); err != nil {
+		log.Fatalf("err: %s", err)
+	}
+
+	if filename != "" {
+		viper.SetConfigFile(filename)
+		if err := viper.MergeInConfig(); err != nil {
+			log.Warnf("loading config file [%s] failed: %s", filename, err)
+		} else {
+			log.Infof("config file [%s] loaded successfully", filename)
+		}
+	}
+
+	viper.SetEnvPrefix("test")
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_", "-", "_"))
+	viper.AutomaticEnv()
+
+	var cfg Config
+	if err := viper.Unmarshal(&cfg); err != nil {
+		log.Fatalf("err: %s", err)
+	}
+
+	return cfg
+}
--- config/default.go
+++ config/default.go
@@ -1,1 +1,6 @@
 package config
+
+const Default = `
+redis:
+  addr: 127.0.0.1:6378
+`
