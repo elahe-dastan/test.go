1879443337d967960cb077a18a5160bb6b150b35
Elahe Dastan <elahe.dstn@gmail.com>
2020-08-12 11:06:24 +0430

    Create redis connection

--- database/redis.go
+++ database/redis.go
@@ -1,1 +1,18 @@
 package database
+
+import (
+	"log"
+	"test/config"
+
+	"github.com/gomodule/redigo/redis"
+)
+
+
+func NewRedisConn(cfg config.Redis) redis.Conn {
+	conn, err := redis.Dial("tcp", cfg.Addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return conn
+}
